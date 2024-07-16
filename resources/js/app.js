7fa4fccf71e8823b7bba2196b01d074830158b49
webspeed2 <seoksoonhwan@gmail.com>
2024-07-16 09:17:41 +0900

    main

---
+++ resources/js/app.js
@@ -0,0 +1,12 @@
+import './bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import { createApp } from 'vue';
+
+import App from './components/App.vue';
+
+
+
+createApp(App)
+    .mount("#app")
---
+++ resources/js/components/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+//import 'particles.js/particles';
+</script>
+
+<template>
+    <h1>Fisrt Component in our Laravel vuejs website </h1>
+    
+    <button type="button" class="btn btn-sm btn-primary">Button</button>
+
+    
+    <header id="auth-header" class="auth-header"></header>
+
+</template>
+
+<script>
+//particlesJS.load('auth-header', {});
+
+</script>
